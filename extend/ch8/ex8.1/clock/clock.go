a58ece569389e487fa3b1904c4c07de4cf6c98d0
gavin.ma <gavin.ma@klook.com>
2019-10-29 23:29:40 +0800

    ch8/ex8.1

--- extend/ch8/ex8.1/clock/clock.go
+++ extend/ch8/ex8.1/clock/clock.go
@@ -1,1 +1,43 @@
-package clock
+package main
+
+import (
+	"flag"
+	"fmt"
+	"io"
+	"log"
+	"net"
+	"time"
+)
+
+// 使用 -port 指定端口
+var port = flag.Int("port", 8080, "listen port")
+
+func main() {
+	flag.Parse()
+	log.Print(*port)
+	listener, err := net.Listen("tcp", fmt.Sprintf("localhost:%d", *port))
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// 死循环
+	for {
+		conn, err := listener.Accept()
+		if err != nil {
+			log.Print(err)
+			continue
+		}
+		go handleConn(conn)
+	}
+}
+
+func handleConn(c net.Conn) {
+	defer c.Close()
+	for {
+		_, err := io.WriteString(c, time.Now().Format("15:04:05\n"))
+		if err != nil {
+			return // e.g., client disconnected
+		}
+		time.Sleep(1 * time.Second)
+	}
+}
