b138419665ea616df618bfdc387941b8bf2017b9
mcrwayfun <mcrwayfun@gmail.com>
2019-10-30 08:10:52 +0800

    ch8/ex8.3

---
+++ extend/ch8/ex8.3/netcat3/netcat.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"io"
+	"log"
+	"net"
+	"os"
+)
+
+func main() {
+	addr, err := net.ResolveTCPAddr("tcp", "localhost:8000")
+	if err != nil {
+		log.Fatal(err)
+	}
+	conn, err := net.DialTCP("tcp", nil, addr)
+	if err != nil {
+		log.Fatal(err)
+	}
+	//defer conn.Close()
+	done := make(chan struct{}) // channel使用make初始化，用struct来构造表示只用来传递消息
+	go func() { // 模拟异步操作
+		_, _ = io.Copy(os.Stdout, conn)
+		log.Println("done")
+		done <- struct{}{} // 发送消息给main goroutine
+	}()
+	mustCopy(conn, os.Stdin)
+	_ = conn.CloseWrite()
+	<-done
+}
+
+func mustCopy(dst io.Writer, src io.Reader) {
+	if _, err := io.Copy(dst, src); err != nil {
+		if err == io.EOF {
+			return
+		}
+		log.Fatal(err)
+	}
+}
--- gopl.io/ch8/reverb1/reverb.go
+++ gopl.io/ch8/reverb1/reverb.go
@@ -10,7 +10,7 @@ import (
 )
 
 func main() {
-	listener, err := net.Listen("tcp", "localhost:8080")
+	listener, err := net.Listen("tcp", "localhost:8000")
 	if err != nil {
 		log.Fatal(err)
 	}
