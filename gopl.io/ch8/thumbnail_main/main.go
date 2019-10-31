133d544e799954889ffe74afe5a228a04d5b9e13
mcrwayfun <mcrwayfun@gmail.com>
2019-10-31 09:24:16 +0800

    ch8/thumbnail

---
+++ gopl.io/ch8/thumbnail_main/main.go
@@ -0,0 +1,15 @@
+package main
+
+func main() {
+	// test1
+	//thumbnail.MakeThumbnails([]string{"gopl.io/ch8/thumbnail/gopher.png"})
+	// test2 没有生成结果就返回
+	//thumbnail.MakeThumbnails2([]string{"gopl.io/ch8/thumbnail/gopher.png"})
+	// test3
+	//thumbnail.MakeThumbnails3([]string{"gopl.io/ch8/thumbnail/gopher.png"})
+	// test4
+	//err := thumbnail.MakeThumbnails4([]string{"gopl.io/ch8/thumbnail/gopher.png"})
+	//if err != nil {
+	//	log.Fatal(err)
+	//}
+}
