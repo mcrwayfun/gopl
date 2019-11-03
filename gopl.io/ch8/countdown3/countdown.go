21e685e20ac69c2da9de6b47c2d6516522aeffa7
mcrwayfun <mcrwayfun@gmail.com>
2019-11-03 11:13:17 +0800

    ch8/countdown3

---
+++ gopl.io/ch8/countdown3/countdown.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"time"
+)
+
+func main() {
+	abort := make(chan struct{})
+	go func() {
+		os.Stdin.Read(make([]byte, 1)) // read a single byte
+		abort <- struct{}{}
+	}()
+
+	fmt.Println("Commencing countdown.  Press return to abort.")
+	tick := time.Tick(1 * time.Second)
+	for countdown := 10; countdown > 0; countdown-- {
+		fmt.Println(countdown)
+		select {
+		case <-tick:
+			// Do nothing.
+			// 当main goroutine结束时，tick依旧试图向channel中发送数据，但此时
+			// 已经没有goroutine会从该channel中接送值了，这会造成goroutine泄漏
+		case <-abort:
+			fmt.Println("Launch aborted!")
+			return
+		}
+	}
+	launch()
+}
+
+func launch() {
+	fmt.Println("Lift off!")
+}
