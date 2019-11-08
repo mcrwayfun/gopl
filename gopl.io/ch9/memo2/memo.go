eb2097409383c6c7ab44ac7578dad8b2ebb5b2a2
mcrwayfun <mcrwayfun@gmail.com>
2019-11-08 23:53:40 +0800

    ch9/memo2

---
+++ gopl.io/ch9/memo2/memo.go
@@ -0,0 +1,47 @@
+package memo
+
+import (
+	"io/ioutil"
+	"net/http"
+	"sync"
+)
+
+type Memo struct {
+	f     Func
+	mu    sync.Mutex
+	cache map[string]result
+}
+
+type Func func(key string) (interface{}, error)
+
+type result struct {
+	value interface{}
+	err   error
+}
+
+func New(f Func) *Memo {
+	return &Memo{
+		f:     f,
+		cache: make(map[string]result),
+	}
+}
+
+func (memo *Memo) Get(key string) (interface{}, error) {
+	memo.mu.Lock()
+	res, ok := memo.cache[key] // recheck
+	if !ok {
+		res.value, res.err = memo.f(key)
+		memo.cache[key] = res
+	}
+	memo.mu.Unlock()
+	return res.value, res.err
+}
+
+func httpGetBody(url string) (interface{}, error) {
+	resp, err := http.Get(url)
+	if err != nil {
+		return nil, err
+	}
+	defer resp.Body.Close()
+	return ioutil.ReadAll(resp.Body)
+}
